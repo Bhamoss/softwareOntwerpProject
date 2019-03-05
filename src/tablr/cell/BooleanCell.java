68a67c4aed99fa9a5e39414681a2583d6fda1941
Bhamoss <26162072+Bhamoss@users.noreply.github.com>
2019-03-05 21:05:28 +0100

    Initiliased test directories

--- src/tablr/cell/BooleanCell.java
+++ src/tablr/cell/BooleanCell.java
@@ -1,4 +1,65 @@
 package tablr.cell;
 
+
+/**
+ *
+ */
 public class BooleanCell extends Cell {
+
+    /**
+     *
+     * @return
+     */
+    public boolean getValue()
+    {
+        return Value;
+    }
+
+    /**
+     *
+     * @param Value
+     * @return
+     */
+    public static Boolean isValidValue(Boolean Value)
+    {
+
+    }
+
+    /**
+     *
+     *
+     */
+    public void setValue()
+    {
+
+    }
+
+    /**
+     *
+     */
+    private boolean Value;
+
+    /**
+     *
+     * @return
+     */
+    public static  Boolean getDefaultValue()
+    {
+        return DefaultValue;
+    }
+
+
+    /**
+     *
+     * @param newValue
+     */
+    public static void setDefaultValue(Boolean newValue)
+    {
+        DefaultValue = newValue;
+    }
+
+    /**
+     *
+     */
+    private static Boolean DefaultValue;
 }
---
+++ test/tablr/TableTest.java
@@ -0,0 +1,7 @@
+package tablr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+
+}
