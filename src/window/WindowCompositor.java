c28a94824f737045a2ebca7c04b3e0c1e83d6b2d
jjcmoon <jaron.maene@gmail.com>
2019-04-15 12:25:39 +0200

    WindowCompositor: implementation of active windows, mouse/key event handling

--- src/window/WindowCompositor.java
+++ src/window/WindowCompositor.java
@@ -1,23 +1,77 @@
 package window;
 
+import window.widget.CompositeWidget;
+
 import java.awt.*;
 import java.util.LinkedList;
 
 public class WindowCompositor extends CanvasWindow {
 
-    LinkedList<SubWindowWidget> subwindows;
+    LinkedList<SubWindowWidget> subWindows;
 
     public WindowCompositor() {
         super("Tablr");
+        this.subWindows = new LinkedList<SubWindowWidget>();
     }
 
 
     public void addSubWindow(SubWindowWidget subwindow) {
-        subwindows.add(subwindow);
+        subWindows.add(subwindow);
+        subWindows.getLast().setActive(false);
+        subwindow.setActive(true);
+
     }
 
     public void removeSubWindow(SubWindowWidget subwindow) {
-        subwindows.remove(subwindow);
+        subWindows.remove(subwindow);
+        subWindows.getLast().setActive(true);
+        subwindow.setActive(false);
     }
 
+    public void setActiveSubWindow(SubWindowWidget subwindow) {
+        removeSubWindow(subwindow);
+        addSubWindow(subwindow);
+    }
+
+    public boolean isSubWindowActive(SubWindowWidget subwindow) {
+        return subwindow.equals(subWindows.getLast());
+    }
+
+    public SubWindowWidget getActiveWindow() {
+        return subWindows.getLast();
+    }
+
+    private SubWindowWidget resolveCoordinate(int x, int y) {
+        for (int i=subWindows.size()-1; i>=0; i--) {
+            if (subWindows.get(i).containsPoint(x, y))
+                return subWindows.get(i);
+        }
+        return null;
+    }
+
+    @Override
+    protected void paint(Graphics g) {
+        for (SubWindowWidget w : subWindows) {
+            w.paint(w);
+        }
+    }
+
+    @Override
+    protected void handleMouseEvent(int id, int x, int y, int clickCount) {
+        SubWindowWidget clickedWindow = resolveCoordinate(x, y);
+        if (clickedWindow == null)
+            return;
+
+        if (clickedWindow.isActive()) {
+            clickedWindow.handleMouseEvent(id, x, y, clickCount);
+        } else {
+            setActiveSubWindow(clickedWindow);
+        }
+
+    }
+
+    @Override
+    protected void handleKeyEvent(int id, int keyCode, char keyChar) {
+        getActiveWindow().handleKeyEvent(id, keyCode, keyChar);
+    }
 }
