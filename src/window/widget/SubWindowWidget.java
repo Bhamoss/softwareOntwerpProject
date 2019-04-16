c45265aeabdef6f51ec6903f75459d4f9536432e
MichielJ1998 <michiel.jonckheere@live.be>
2019-04-16 11:31:08 +0200

    SubWindowWidget addWidget and setPosition override

--- src/window/widget/SubWindowWidget.java
+++ src/window/widget/SubWindowWidget.java
@@ -16,4 +16,22 @@ public class SubWindowWidget extends ComponentWidget {
     public boolean isActive() {
         return isActive;
     }
+
+    @Override
+    public void addWidget(Widget w) {
+        // position widget wordt tov de positie van deze subwindow gepositioneerd
+        w.setPosition(getX() + w.getX(), getY() + w.getY());
+        super.addWidget(w);
+    }
+
+    @Override
+    protected void setPosition(int x, int y) {
+        // eerst van elke widget de huidige positie aftrekken (positie vn widget tov punt (0,0) )
+        //      en dan optellen met de nieuwe (x,y) positie (positie vn widget tov van punt (x,y)),
+        //      pas daarna de positie van de subwindow herinstellen
+        for (Widget w:widgets) {
+            w.setPosition(w.getX() - getX() + x, w.getY() - getY() + y);
+        }
+        super.setPosition(x, y);
+    }
 }
