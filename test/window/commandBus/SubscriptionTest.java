ffed6abba5fdd73a663e43b9411079a28ef4c6e1
Bhamoss <26162072+Bhamoss@users.noreply.github.com>
2019-04-18 00:37:51 +0200

    Forgot to push the test files of Subscription.

---
+++ test/window/commandBus/SubscriptionTest.java
@@ -0,0 +1,187 @@
+package window.commandBus;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubscriptionTest {
+
+    Subscription basicSub = null;
+    SubWidget otherWidget = null;
+    SubCommand basicCommand = null;
+    SubWidget basicWidget = null;
+    BrotherWidget brotherWidget = null;
+    BrotherCommand brotherCommand = null;
+    SubclassSubWidget subclassSubWidget = null;
+
+    Method noSubscribe = null;
+    Method twoMethod = null;
+    Method intMethod = null;
+    Method privMethod = null;
+    Method validMethod = null;
+    Method brotherMethod = null;
+
+    String f = null;
+
+    @BeforeEach
+    void setUp() {
+        basicWidget = new SubWidget();
+        Method[] methods = basicWidget.getClass().getDeclaredMethods();
+        for (Method method:
+             methods) {
+            f = method.getName();
+            if (method.getName().equals("execute"))
+            {
+                noSubscribe = method;
+            }
+            else if (method.getName().equals("privMethod"))
+            {
+                privMethod = method;
+            }
+            else if (method.getName().equals("twoMethod"))
+            {
+                twoMethod = method;
+            }
+            else if (method.getName().equals("intMethod"))
+            {
+                intMethod = method;
+            }
+            else
+            {
+                validMethod = method;
+            }
+        }
+        basicSub = new Subscription(basicWidget, validMethod);
+        otherWidget = new SubWidget();
+        basicCommand = new SubCommand();
+        brotherWidget = new BrotherWidget();
+        methods = brotherWidget.getClass().getDeclaredMethods();
+        for (Method method:
+                methods) {
+            f = method.getName();
+            noSubscribe = null;
+            if (method.getName().equals("notSubWidgetMethod"))
+            {
+                brotherMethod = method;
+            }
+        }
+        brotherCommand = new BrotherCommand();
+        subclassSubWidget = new SubclassSubWidget();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+
+    /*
+     * CONSTRUCTOR
+     */
+
+    @Test
+    @DisplayName("Constructor subscriber null.")
+    void constructorSubscriberNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(null, noSubscribe));
+    }
+
+    @Test
+    @DisplayName("Constructor onEvent null.")
+    void constructorOnEventNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, null));
+    }
+
+    @Test
+    @DisplayName("Constructor onEvent no annotation.")
+    void constructorOnEventNoAnnotation() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, noSubscribe));
+    }
+
+    @Test
+    @DisplayName("Constructor onEvent not public.")
+    void constructorOnEventNotPublic() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, privMethod));
+    }
+
+
+    @Test
+    @DisplayName("Constructor onEvent 2 parameters.")
+    void constructorOnEventTwoParams() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, twoMethod));
+    }
+
+    @Test
+    @DisplayName("Constructor onEvent parameter not UICommand.")
+    void constructorOnEventNotUICommand() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, intMethod));
+    }
+
+    @Test
+    @DisplayName("Constructor onEvent not a method reachable by the widget.")
+    void constructorOnEventNotReachable() {
+        assertThrows(IllegalArgumentException.class, () -> new Subscription(basicWidget, brotherMethod));
+    }
+
+
+    @Test
+    @DisplayName("Constructor success superclass method.")
+    void constructorSuccessParentListen() {
+        Subscription s = new Subscription(subclassSubWidget, validMethod);
+    }
+
+
+    //success is basically tested at the beginning
+
+
+    /*
+     * void trigger(UICommand command) throws IllegalArgumentException
+     */
+
+
+    @Test
+    @DisplayName("Trigger null IllegalArgumentException.")
+    void triggerNull() {
+        assertThrows(IllegalArgumentException.class, () -> basicSub.trigger(null));
+    }
+
+    @Test
+    @DisplayName("Trigger not a parameter subclass IllegalArgumentException.")
+    void triggerNoUICommand() {
+        assertThrows(IllegalArgumentException.class, () -> basicSub.trigger(brotherCommand));
+    }
+
+    @Test
+    @DisplayName("Trigger success.")
+    void triggerSuccess() {
+        basicSub.trigger(basicCommand);
+        assertEquals(basicCommand.t, basicWidget.testvar);
+    }
+
+    /*
+     * boolean isSubscriber(Widget widget)
+     */
+
+
+    @Test
+    @DisplayName("isSubscriber null")
+    void isSubscriberNull() {
+        assertFalse(basicSub.isSubscriber(null));
+    }
+
+    @Test
+    @DisplayName("isSubscriber not the widget")
+    void isSubscriberOtherWidget() {
+        assertFalse(basicSub.isSubscriber(otherWidget));
+    }
+
+    @Test
+    @DisplayName("isSubscriber success")
+    void isSubscriberSuccess() {
+        assertTrue(basicSub.isSubscriber(basicWidget));
+    }
+}
---
+++ test/window/commandBus/SubscriptionTestClasses.java
@@ -0,0 +1,90 @@
+package window.commandBus;
+
+import window.widget.Widget;
+import window.commands.UICommand;
+
+
+class SubWidget extends Widget
+{
+
+    //MUST STAY THE FIRST METHOD
+    public void execute() {
+
+    }
+
+    // MUST STAY SECOND
+    @Subscribe
+    private void privMethod()
+    {
+
+    }
+
+    // MUST STAY THIRD
+    @Subscribe
+    public void twoMethod(int i, int j)
+    {
+
+    }
+
+    // MUST STAY FOURTH
+    @Subscribe
+    public void intMethod(int i)
+    {
+
+    }
+
+    // MUST STAY FIFTH
+    @Subscribe
+    public void validMethod(SubCommand i)
+    {
+        this.testvar = i.t;
+    }
+
+    public int testvar = 0;
+
+    SubWidget()
+    {
+        super(1,1,1,1,true);
+    }
+}
+
+class SubclassSubWidget extends SubWidget
+{
+    public int m = 1;
+}
+
+class BrotherWidget extends Widget
+{
+
+    @Subscribe
+    public void notSubWidgetMethod(UICommand command)
+    {
+
+    }
+
+    BrotherWidget()
+    {
+        super(1,1,1,1,true);
+    }
+}
+
+class SubCommand extends UICommand
+{
+
+    @Override
+    public void execute() {
+
+    }
+
+    public int t = 69;
+}
+
+class BrotherCommand extends UICommand
+{
+
+    @Override
+    public void execute() {
+
+    }
+
+}
