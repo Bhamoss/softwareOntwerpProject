0d0225a27783b4facae09720fcc7b5ea3947b6af
MichielJ1998 <michiel.jonckheere@live.be>
2019-04-20 11:43:09 +0200

    nog wat commentaartjes geschreven

--- src/window/widget/TableWidget.java
+++ src/window/widget/TableWidget.java
@@ -12,6 +12,13 @@ public class TableWidget extends CompositeWidget {
     protected LinkedList<ColumnWidget> columnWidgets;
 
 
+    /**
+     * creates a tableWidget
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     */
     public TableWidget(int x, int y, int width, int height) {
         super(x, y, width, height, false);
         assert(height>=25);
@@ -21,6 +28,13 @@ public class TableWidget extends CompositeWidget {
         columnWidgets = new LinkedList<>();
     }
 
+    /**
+     * add a columnWidget to this tableWidget with the given width, resizability and columnName
+     *  if the width of the columnWidget cannot fit in the table, the table width is updated
+     * @param width
+     * @param resizable
+     * @param name
+     */
     public void addColumn(int width, boolean resizable, String name) {
         // Check if space is available
         if (occupancy+width > this.getWidth())
@@ -36,6 +50,11 @@ public class TableWidget extends CompositeWidget {
         //super.addWidget(c);
     }
 
+    /**
+     * adds an entry to the lastAdded columnWidget,
+     *   the order of calling this method is important
+     * @param w
+     */
     public void addEntry(Widget w) {
         columnWidgets.get(lastAdded).addWidget(w);
         if (getHeight() < w.getY() + w.getHeight())
@@ -43,6 +62,10 @@ public class TableWidget extends CompositeWidget {
         lastAdded = (lastAdded + 1) % columnWidgets.size();
     }
 
+    /**
+     * called by a column if he is resized,
+     *      updates the x-values of all the columnWidgets
+     */
     private void resizedColumn() {
         int x = this.getX();
         occupancy = 0;
@@ -54,6 +77,10 @@ public class TableWidget extends CompositeWidget {
         this.setWidth(occupancy);
     }
 
+    /**
+     * paint all the columnWidgets of this tableWidget
+     * @param g
+     */
     @Override
     protected void paintWidgets(Graphics g) {
         for (Widget w: columnWidgets) {
@@ -61,17 +88,42 @@ public class TableWidget extends CompositeWidget {
         }
     }
 
-
+    /**
+     * Sets the given x as x-value for this tableWidget
+     *      Also updates the x-value for all the columnWidgets inside this tableWidget
+     * @param x
+     */
     @Override
-    protected void setPosition(int x, int y) {
-        super.setPosition(x, y);
+    public void setX(int x) {
+        super.setX(x);
         occupancy = 0;
         for (ColumnWidget w: columnWidgets) {
-            w.setPosition(getX() + occupancy, getY());
+            w.setX(getX() + occupancy);
             occupancy += w.getWidth() + 1;
         }
     }
 
+    /**
+     * Sets the given y as y-value for this tableWidget
+     *      Also updates the y-value for all the columnWidgets inside this tableWidget
+     * @param y
+     */
+    @Override
+    public void setY(int y) {
+        super.setY(y);
+        if (columnWidgets == null) return;
+        for(ColumnWidget w: columnWidgets) {
+            w.setY(y);
+        }
+    }
+
+    /**
+     * call the handleMouseEvent on all the columnWidgets in this tableWidget
+     * @param id
+     * @param x
+     * @param y
+     * @param clickCount
+     */
     @Override
     public boolean handleMouseEvent(int id, int x, int y, int clickCount) {
         boolean r = false;
