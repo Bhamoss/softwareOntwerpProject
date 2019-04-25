7005c1fab32017e8f9c822e2167892ad25464c52
MichielJ1998 <michiel.jonckheere@live.be>
2019-04-25 18:52:28 +0200

    voorstel switcher columns

--- src/tablr/ColumnConverter.java
+++ src/tablr/ColumnConverter.java
@@ -4,7 +4,11 @@ import tablr.column.*;
 
 public class ColumnConverter {
 
-    public static StringColumn convertToStringColumn(IntegerColumn c) {
+    public static StringColumn convertToStringColumn(Column c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
         StringColumn result = new StringColumn(c.getId(), c.getName(), c.getNbValues(),
                                                 c.getDefaultValue(), c.isBlanksAllowed());
         for (int i = 1; i <= result.getNbValues(); i++) {
@@ -13,11 +17,91 @@ public class ColumnConverter {
         return result;
     }
 
-    public static StringColumn convertToStringColumn(EmailColumn c) {
-        return null;
+    public static EmailColumn convertToEmailColumn(Column c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
+        EmailColumn result = new EmailColumn(c.getId(), c.getName(), c.getNbValues(),
+                c.getDefaultValue(), c.isBlanksAllowed());
+        for (int i = 1; i <= result.getNbValues(); i++) {
+            result.setValueAt(i, c.getValueAt(i));
+        }
+        return result;
     }
 
-    public static StringColumn convertToStringColumn(BooleanColumn c) {
-        return null;
+    public static IntegerColumn convertToIntegerColumn(BooleanColumn c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
+        IntegerColumn result = new IntegerColumn(c.getId(), c.getName(), c.getNbValues(),
+                booleanToInt(c.getDefaultValue()), c.isBlanksAllowed());
+        for (int i = 1; i <= result.getNbValues(); i++) {
+            result.setValueAt(i, booleanToInt(c.getValueAt(i)));
+        }
+        return result;
     }
+
+    public static IntegerColumn convertToIntegerColumn(Column c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
+        IntegerColumn result = new IntegerColumn(c.getId(), c.getName(), c.getNbValues(),
+                c.getDefaultValue(), c.isBlanksAllowed());
+        for (int i = 1; i <= result.getNbValues(); i++) {
+            result.setValueAt(i, c.getValueAt(i));
+        }
+        return result;
+    }
+
+    public static BooleanColumn convertToBooleanColumn(IntegerColumn c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
+        BooleanColumn result = new BooleanColumn(c.getId(), c.getName(), c.getNbValues(),
+                intToBoolean(c.getDefaultValue()), c.isBlanksAllowed());
+        for (int i = 1; i <= result.getNbValues(); i++) {
+            result.setValueAt(i, intToBoolean(c.getValueAt(i)));
+        }
+        return result;
+    }
+
+    public static BooleanColumn convertToBooleanColumn(Column c) throws IllegalColumnException {
+        if (!c.canHaveAsType("String")) {
+            throw new IllegalColumnException();
+        }
+
+        BooleanColumn result = new BooleanColumn(c.getId(), c.getName(), c.getNbValues(),
+                c.getDefaultValue(), c.isBlanksAllowed());
+        for (int i = 1; i <= result.getNbValues(); i++) {
+            result.setValueAt(i, c.getValueAt(i));
+        }
+        return result;
+    }
+
+    private static String booleanToInt(String b) {
+        if (b.equals("True")) {
+            return "1";
+        } else if (b.equals("False")) {
+            return "0";
+        } else if (b.equals(""))
+            return "";
+        else throw new IllegalArgumentException("Argument must be 'True', 'False' or the empty string.");
+    }
+
+    private static String intToBoolean(String i) {
+        if (i.equals("1")) {
+            return "True";
+        } else if (i.equals("0")) {
+            return "False";
+        } else if (i.equals(""))
+            return "";
+        else throw new IllegalArgumentException("Argument must be '1', '0' or the empty string.");
+    }
+
+
+
 }
