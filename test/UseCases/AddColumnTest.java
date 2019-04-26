b2ddc79a25f5e4a10d68631aa3824875957b9102
Icare433 <icare433@gmail.com>
2019-04-26 14:59:45 +0200

    PushCommands done

---
+++ test/UseCases/AddColumnTest.java
@@ -0,0 +1,19 @@
+package UseCases;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AddColumnTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getColumnNames() {
+    }
+}
