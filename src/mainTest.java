13e05cfe101f086a4c31e6b596e71035904d70bc
MichielJ1998 <33574375+MichielJ1998@users.noreply.github.com>
2019-04-26 13:14:27 +0200

    Jaron beetje helpjen

--- src/mainTest.java
+++ src/mainTest.java
@@ -1,3 +1,4 @@
+import ui.CanvasWindow;
 import ui.UIStarter;
 
 import java.lang.reflect.InvocationTargetException;
@@ -17,10 +18,23 @@ public class mainTest {
             e.printStackTrace();
         }
 
+//        try {
+//            java.awt.EventQueue.invokeAndWait(() -> {
+//                // Start UI
+//                CanvasWindow.replayRecording("C:\\Users\\michi\\Desktop\\AddRow\\Setup.txt", uiStarter.getCompositor());
+//                CanvasWindow.replayRecording("C:\\Users\\michi\\Desktop\\AddRow\\AddRow.txt", uiStarter.getCompositor());
+//
+//            });
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
+
         try {
             java.awt.EventQueue.invokeAndWait(() -> {
                 // Start UI
-                uiStarter.getCompositor().recordSession("C:\\Users\\Provoost\\Desktop\\TestsCode\\AddColumn\\AddColumn.txt");
+                uiStarter.getCompositor().recordSession("C:\\Users\\michi\\Desktop\\EditRowValue\\EditRowValue.txt");
             });
         } catch (InterruptedException e) {
             e.printStackTrace();
